6794531be607ff706c491e5f4a280eff344841d4
DarttGoblin <112183975+DarttGoblin@users.noreply.github.com>
2024-08-07 20:21:33 +0100

    Update History.js

--- History.js
+++ History.js
@@ -14,32 +14,33 @@ app.use((req, res, next) => {
     next();
 });
 
+// Create a connection pool
+const pool = mysql.createPool({
+    connectionLimit: 10, // Adjust the limit as needed
+    host: 'sql7.freesqldatabase.com',
+    user: 'sql7724126',
+    password: 'V6PCDXyNdv',
+    database: 'sql7724126'
+});
+
 app.post("/", (req, res) => {
-    var messages;
-
-    const connection = mysql.createConnection({
-        host: 'sql7.freesqldatabase.com',
-        user: 'sql7724126',
-        password: 'V6PCDXyNdv',
-        database: 'sql7724126'
-    });
-
-    connection.connect((err) => {
+    pool.getConnection((err, connection) => {
         if (err) {
-            console.error('Error connecting to database: ' + err.stack);
-            res.status(500).json({ success: false, error: 'Error connecting to the database, ' + err.stack});
+            console.error('Error getting connection from pool: ' + err.stack);
+            res.status(500).json({ success: false, error: 'Error connecting to the database, ' + err.stack });
             return;
         }
         console.log('Connected to database with connection id ' + connection.threadId);
 
         connection.query('SELECT * FROM conversation0', (error, results) => {
+            connection.release(); // Release the connection back to the pool
+
             if (error) {
                 console.log('Error ' + error.stack);
                 res.status(500).json({ success: false, error: error.stack });
                 return;
             }
-            messages = results;
-            res.status(200).json({ success: true, messages });
+            res.status(200).json({ success: true, messages: results });
         });
     });
 });
